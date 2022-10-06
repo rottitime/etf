33591d16999cdd339911732f30217a2d29288578
Ed Singleton <singletoned@gmail.com>
2022-10-06 11:52:38 +0100

    Add page views

--- business_case_approval/rbca/views.py
+++ business_case_approval/rbca/views.py
@@ -1,23 +1,48 @@
-from django.conf import settings
+from django import forms
+from django.contrib.auth import login
+from django.forms.models import model_to_dict
+from django.http import Http404
 from django.shortcuts import redirect, render
-from django.views.decorators.http import require_http_methods
+from django.urls import reverse
 
-from . import models
+page_order = (
+    "index",
+)
 
-
-@require_http_methods(["GET"])
-def index_view(request):
-    return render(
-        request,
-        template_name="index.html",
-        context={"request": request},
-    )
+view_map = {}
 
 
-@require_http_methods(["GET"])
-def homepage_view(request):
-    return render(
-        request,
-        template_name="homepage.html",
-        context={"request": request},
-    )
+def register(name):
+    def _inner(func):
+        view_map[name] = func
+        return func
+
+    return _inner
+
+
+def page_view(request, page_name="index"):
+    if page_name not in page_order:
+        raise Http404()
+
+    index = page_order.index(page_name)
+    prev_page = index and page_order[index - 1] or None
+    next_page = (index < len(page_order) - 1) and page_order[index + 1] or None
+    prev_url = prev_page and reverse("pages", args=(prev_page,))
+    this_url = reverse("pages", args=(page_name,))
+    next_url = next_page and reverse("pages", args=(next_page,))
+
+    url_data = {
+        "index": index,
+        "prev_page": prev_page,
+        "next_page": next_page,
+        "prev_url": prev_url,
+        "this_url": this_url,
+        "next_url": next_url,
+    }
+    return view_map[page_name](request, url_data)
+
+
+
+@register("index")
+def index_view(request, url_data):
+    return render(request, "index.html")
--- business_case_approval/urls.py
+++ business_case_approval/urls.py
@@ -4,8 +4,8 @@ from django.urls import include, path
 from business_case_approval.rbca import views
 
 urlpatterns = [
-    path("", views.index_view, name="index"),
-    path("home/", views.homepage_view, name="homepage"),
+    path("", views.page_view, name="pages-index"),
+    path("<str:page_name>", views.page_view, name="pages"),
     path("admin/", admin.site.urls),
     path("accounts/", include("allauth.urls")),
 ]
