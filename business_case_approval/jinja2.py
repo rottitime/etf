e9a4ea9b4978353a96a2c9ecd7511e75d768c66b
Ed Singleton <singletoned@gmail.com>
2022-10-08 15:31:29 +0100

    Use Markdown It instead of markdown

--- business_case_approval/jinja2.py
+++ business_case_approval/jinja2.py
@@ -1,15 +1,16 @@
 import jinja2
 from django.templatetags.static import static
 from django.urls import reverse
-import markdown
+from markdown_it import MarkdownIt
 import pypugjs
 
-
+markdown_converter = MarkdownIt()
 
 
 @pypugjs.register_filter('markdown')
-def _markdown(text, ast):
-  return markdown.markdown(text)
+def markdown(text, ast):
+    return markdown_converter.render(text)
+
 
 def environment(**options):
     extra_options = dict()
