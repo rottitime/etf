d3387b4b0715d544af214fb38b9e04533cfdd992
Nina Menezes <nina.menezes@example.com>
2022-11-17 17:00:55 +0000

    add some longer descriptions

--- etf/evaluation/admin.py
+++ etf/evaluation/admin.py
@@ -4,3 +4,6 @@ from . import models
 
 admin.site.register(models.User)
 admin.site.register(models.Evaluation)
+admin.site.register(models.Intervention)
+admin.site.register(models.OutcomeMeasure)
+admin.site.register(models.OtherMeasure)
