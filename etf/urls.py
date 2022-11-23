e15c4e05ff5bd3da28214ed067c718aa2481d3a2
Ed Singleton <singletoned@gmail.com>
2022-11-23 11:14:42 +0000

    Fix uuid in urls

--- etf/urls.py
+++ etf/urls.py
@@ -5,8 +5,8 @@ from etf.evaluation import views
 
 urlpatterns = [
     path("", views.index_view, name="index"),
-    path("evaluation/<int:evaluation_id>/", views.page_view, name="pages-index"),
-    path("evaluation/<int:evaluation_id>/<str:page_name>", views.page_view, name="pages"),
+    path("evaluation/<uuid:evaluation_id>/", views.page_view, name="pages-index"),
+    path("evaluation/<uuid:evaluation_id>/<str:page_name>", views.page_view, name="pages"),
     path("admin/", admin.site.urls),
     path("accounts/", include("allauth.urls")),
 ]
