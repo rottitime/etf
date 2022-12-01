9d07917cdab5d77e195c1229151c0dbd97a10a98
Ed Singleton <singletoned@gmail.com>
2022-12-01 21:46:25 +0000

    Add a basic test

---
+++ tests/test_pages.py
@@ -0,0 +1,24 @@
+import functools
+
+import httpx
+
+import etf.wsgi
+
+TEST_SERVER_URL = "http://etf-testserver:8010/"
+
+
+def with_client(func):
+    @functools.wraps(func)
+    def _inner(*args, **kwargs):
+        with httpx.Client(app=etf.wsgi.application, base_url=TEST_SERVER_URL) as client:
+            return func(client, *args, **kwargs)
+
+    return _inner
+
+
+@with_client
+def test_add_evaluation(client):
+    response = client.get("/")
+    print(response)
+    print(response.text)
+    assert response.status_code == 200
