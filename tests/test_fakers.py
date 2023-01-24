ac7bbe034dbc8317df7c6d9b65baea55fb39e948
Nina Menezes <nina.menezes@example.com>
2023-01-24 15:30:12 +0000

    add more tests

--- tests/test_fakers.py
+++ tests/test_fakers.py
@@ -2,4 +2,5 @@ from etf.evaluation.fakers import add_users
 
 
 def test_add_users():
-    
+    users = add_users(5)
+    assert len(list(users)) == 5
--- tests/test_schemas.py
+++ tests/test_schemas.py
@@ -14,9 +14,9 @@ def test_date_and_blank_field():
     deserialized_obj = schema.load({"date": "2022-11-13"})
     assert deserialized_obj["date"] == date(2022, 11, 13)
     deserialized_obj = schema.load({"date": ""})
-    assert deserialized_obj["date"] == None
+    assert deserialized_obj["date"] is None
     deserialized_obj = schema.load({"date": None})
-    assert deserialized_obj["date"] == None
+    assert deserialized_obj["date"] is None
 
 
 # TODO - add more tests for schemas, esp after validation added
