b2a866c11c2c654f9533de65e403dc99de36409c
Elliot Moore <elliotmoorecornwall@outlook.com>
2023-01-31 15:20:57 +0000

    Initial changes to add sentry to etf

--- cfscripts/etf-envs.sh
+++ cfscripts/etf-envs.sh
@@ -4,6 +4,17 @@ if [ $1 ]; then
     CF_SPACE=$1
 fi
 
+sentry_envs=(
+    prod
+    develop
+    staging
+    pentest
+)
+
+if [[ " ${sentry_envs[*]} " =~ " ${CF_SPACE} " ]]; then
+  sentry=true
+fi
+
 while read -r line; do
 apps=$(echo $line | head -n1 | cut -d " " -f1)
 if [ "$apps" != "Getting" ] && [ "$apps" != "name" ] && [ "$apps" ]; then
@@ -28,6 +39,10 @@ do
             $(./cf set-env ${value} DJANGO_SECRET_KEY ${PROD_DJANGO_SECRET_KEY} &> /dev/null)
         else
             $(./cf set-env ${value} DJANGO_SECRET_KEY ${DJANGO_SECRET_KEY} &> /dev/null)
+        fi
+        if [ $autoscale ]; then
+            $(./cf set-env ${value} SENTRY_DSN ${SENTRY_DSN}  &> /dev/null)
+            $(./cf set-env ${value} SENTRY_ENVIRONMENT ${CF_SPACE} &> /dev/null)
         fi
     fi
 done
