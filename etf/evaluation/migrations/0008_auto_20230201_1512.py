b92784b94870941519208d7f572b6d608237089e
Elliot Moore <elliotmoorecornwall@outlook.com>
2023-02-01 16:20:49 +0000

    Fixed single quotes to double quotes in auto-generated migration file

--- etf/evaluation/migrations/0008_auto_20230201_1512.py
+++ etf/evaluation/migrations/0008_auto_20230201_1512.py
@@ -6,17 +6,17 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('evaluation', '0007_auto_20230120_1434'),
+        ("evaluation", "0007_auto_20230120_1434"),
     ]
 
     operations = [
         migrations.RemoveField(
-            model_name='evaluation',
-            name='is_published',
+            model_name="evaluation",
+            name="is_published",
         ),
         migrations.AddField(
-            model_name='evaluation',
-            name='status',
-            field=models.CharField(blank=True, choices=[('DRAFT', 'Draft'), ('CIVIL_SERVICE', 'Civil Service'), ('PUBLIC', 'Public')], max_length=256),
+            model_name="evaluation",
+            name="status",
+            field=models.CharField(blank=True, choices=[("DRAFT", "Draft"), ("CIVIL_SERVICE", "Civil Service"), ("PUBLIC", "Public")], max_length=256),
         ),
     ]
