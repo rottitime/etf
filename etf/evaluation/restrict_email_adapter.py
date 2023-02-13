8a21349b96575eced1e18238c28b3c169d9cb9f5
Elliot Moore <elliotmoorecornwall@outlook.com>
2023-02-13 15:02:28 +0000

    Change email adapter to use system settings for allowed domains

--- etf/evaluation/restrict_email_adapter.py
+++ etf/evaluation/restrict_email_adapter.py
@@ -1,17 +1,12 @@
-import environ
 from allauth.account.adapter import DefaultAccountAdapter
+from django.conf import settings
 from django.forms import ValidationError
-
-env = environ.Env()
 
 
 class RestrictEmailAdapter(DefaultAccountAdapter):
     def clean_email(self, email):
-        allowed_domains = ["cabinetoffice.gov.uk"]
         domain = email.split("@")[-1]
-        allow_example_domain = env.bool("ALLOW_EXAMPLE_EMAILS", default=False)
-        if allow_example_domain:
-            allowed_domains.append("example.com")
+        allowed_domains = settings.ALLOWED_DOMAINS
         if domain not in allowed_domains:
             raise ValidationError(
                 "This email domain is not yet supported. Please contact the site admin team if you think this is incorrect."
