6c58e28173b53adb58f78ddae1330d9e73b75c63
Ed Singleton <singletoned@gmail.com>
2023-02-16 14:04:56 +0000

    Add scraping script

---
+++ scripts/scrape_organisations.py
@@ -0,0 +1,39 @@
+import json
+import pathlib
+
+import httpx
+
+__here__ = pathlib.Path(__file__).parent
+DATA_DIR = __here__ / ".." / "data"
+
+base_url = "https://www.gov.uk/api/organisations"
+
+client = httpx.Client()
+
+
+def get_results_and_next_url(url):
+    response = client.get(url)
+    data = response.json()
+    links = {item["rel"]: item["href"] for item in data["_response_info"]["links"]}
+    next_url = links.get("next", None)
+    results = data.get("results", [])
+    return results, next_url
+
+
+def gather_results(start_url):
+    next_url = start_url
+    while next_url:
+        results, next_url = get_results_and_next_url(next_url)
+        for result in results:
+            yield result
+
+
+def main():
+    results = list(gather_results(base_url))
+    output_filename = DATA_DIR / "organisations.json"
+    with output_filename.open("w") as f:
+        json.dump(results, f)
+
+
+if __name__ == "__main__":
+    main()
