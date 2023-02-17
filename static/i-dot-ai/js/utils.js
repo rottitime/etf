f1079c3dc3f22718defc812e6f4173efc6ae90ab
Elliot Moore <elliotmoorecornwall@outlook.com>
2023-02-17 11:33:11 +0000

    Remove enter button js for search input

--- static/i-dot-ai/js/utils.js
+++ static/i-dot-ai/js/utils.js
@@ -1,11 +1,3 @@
 $(document).ready(function() {
   $(document).foundation();
 });
-
-$(document).ready(function() {
-    $('#search_phrase').keypress(function(e) {
-      if (e.which == 13) {
-        $(this).closest('form').submit();
-      }
-    });
-  });
