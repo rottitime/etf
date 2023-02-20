4edab589335155d4f10492283709139210b6c3ce
Ed Singleton <singletoned@gmail.com>
2023-02-20 09:16:50 +0000

    Make id a primary_key

--- etf/evaluation/models.py
+++ etf/evaluation/models.py
@@ -11,7 +11,7 @@ class UUIDPrimaryKeyBase(models.Model):
     class Meta:
         abstract = True
 
-    id = models.UUIDField(default=uuid.uuid4, editable=False)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
 
 
 class User(BaseUser):
