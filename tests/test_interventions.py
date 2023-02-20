fbac682daa76dcd7aea007b7092ac6454ca8e11d
Ed Singleton <singletoned@gmail.com>
2023-02-20 15:19:55 +0000

    Add test for interventions

---
+++ tests/test_interventions.py
@@ -0,0 +1,25 @@
+from . import utils
+
+USER_DATA = {"email": "mr_interventions_test@example.com", "password": "1-h4t3-p455w0rd-c0mpl3xity-53tt1ng5"}
+
+
+def test_intervention():
+    client = utils.make_testino_client()
+    utils.register(client, **USER_DATA)
+
+    page = client.get("/")
+    assert page.status_code == 200, page.status_code
+
+    form = page.get_form('''form[action="/"]''')
+    page = form.submit().follow()
+    assert page.status_code == 200, page.status_code
+
+    page = page.click(contains="Next")
+    assert page.status_code == 200, page.status_code
+    assert page.url.endswith("/title")
+
+    intervention_url = "/".join(page.url.split("/")[:-1]+["interventions/"])
+    page = client.get(intervention_url)
+
+    form = page.get_form('''#intervention-add-form''')
+    page = form.submit().follow()
