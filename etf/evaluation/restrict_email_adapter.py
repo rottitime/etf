45ecbe9870d343823c3b7c4e752cf61fb7c64a47
Nina Menezes <nina.menezes@cabinetoffice.gov.uk>
2023-02-21 11:22:08 +0000

    change to check for gov.uk emails

--- etf/evaluation/restrict_email_adapter.py
+++ etf/evaluation/restrict_email_adapter.py
@@ -3,11 +3,30 @@ from django.conf import settings
 from django.forms import ValidationError
 
 
+def is_gov_uk(email):
+    allowed = False
+    email = email.lower()
+    email_split = email.split(".")
+    if len(email_split) > 1:
+        if email_split[-2] == "gov" and email_split[-1] == "uk":
+            allowed = True
+    return allowed
+
+
 class RestrictEmailAdapter(DefaultAccountAdapter):
     def clean_email(self, email):
-        domain = email.split("@")[-1]
-        allowed_domains = settings.ALLOWED_DOMAINS
-        if domain not in allowed_domains:
+        print(email)
+        email = email.lower()
+        email_allowed = False
+        email_allowed = is_gov_uk(email)
+        print(1)
+        print(email_allowed)
+        if not email_allowed:
+            domain = email.split("@")[-1]
+            email_allowed = domain in settings.ALLOWED_DOMAINS
+            print(2)
+            print(email_allowed)
+        if not email_allowed:
             raise ValidationError(
                 "This email domain is not yet supported. Please contact the site admin team if you think this is incorrect."
             )
