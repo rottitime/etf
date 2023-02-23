472c2f78d2b1a1693bbe76e4506e3e754b340f32
Ed Singleton <singletoned@gmail.com>
2023-02-23 20:28:03 +0000

    Change domain in registration test

--- tests/test_registration.py
+++ tests/test_registration.py
@@ -1,6 +1,6 @@
 from . import utils
 
-USER_DATA = {"email": "mr_registration_test@example.com", "password": "1-h4t3-p455w0rd-c0mpl3xity-53tt1ng5"}
+USER_DATA = {"email": "mr_registration_test@not-a-domain.com", "password": "1-h4t3-p455w0rd-c0mpl3xity-53tt1ng5"}
 
 
 def test_emails():
