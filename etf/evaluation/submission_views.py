fdccfe9f20685190ea6b2ad431cc56aff13bf1fa
Ed Singleton <singletoned@gmail.com>
2023-03-06 12:30:48 +0000

    Remove unused import

--- etf/evaluation/submission_views.py
+++ etf/evaluation/submission_views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect, render
 from django.urls import reverse
 
-from . import enums, models, pages, schemas
+from . import models, pages, schemas
 
 
 @login_required
