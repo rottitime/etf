9f7f5088f2ab23474b87933f94516d4ed306a710
Ed Singleton <singletoned@gmail.com>
2023-03-06 17:24:41 +0000

    Add get to evaluation facade

--- etf/evaluation/interface.py
+++ etf/evaluation/interface.py
@@ -8,6 +8,11 @@ class CreateEvaluationSchema(marshmallow.Schema):
     user_id = marshmallow.fields.UUID()
 
 
+class GetEvaluationSchema(marshmallow.Schema):
+    user_id = marshmallow.fields.UUID()
+    evaluation_id = marshmallow.fields.UUID()
+
+
 class Evaluation(Entity):
     @with_schema(load=CreateEvaluationSchema, dump=schemas.EvaluationSchema)
     @register_event("Evaluation created")
@@ -17,6 +22,11 @@ class Evaluation(Entity):
         evaluation.save()
         evaluation.users.add(user)
         evaluation.save()
+        return evaluation
+
+    @with_schema(load=GetEvaluationSchema, dump=schemas.EvaluationSchema)
+    def get(self, user_id, evaluation_id):
+        evaluation = models.Evaluation.objects.get(id=evaluation_id, users__id=user_id)
         return evaluation
 
 
--- tests/test_interface.py
+++ tests/test_interface.py
@@ -3,8 +3,13 @@ from etf.evaluation import interface, models
 USER_DATA = {"email": "mr_interface_test@example.com", "password": "1-h4t3-p455w0rd-c0mpl3xity-53tt1ng5"}
 
 
-def test_evaluation_created():
+def test_evaluation_facade():
     user, _ = models.User.objects.get_or_create(email=USER_DATA["email"])
     result = interface.facade.evaluation.create(user_id=user.id)
     expected = [{"email": "mr_interface_test@example.com"}]
+    assert result["users"] == expected
+
+    evaluation_id = result["id"]
+    result = interface.facade.evaluation.get(user_id=user.id, evaluation_id=evaluation_id)
+
     assert result["users"] == expected
