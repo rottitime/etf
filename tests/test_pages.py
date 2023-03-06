1d53887937defa433e8e85188da476a9203566bf
Ed Singleton <singletoned@gmail.com>
2023-03-06 13:02:22 +0000

    Fix tests

--- tests/test_pages.py
+++ tests/test_pages.py
@@ -15,8 +15,15 @@ def test_get_prev_next_page_name_middle():
     assert result == expected, result
 
 
-def test_get_prev_next_page_name_last():
+def test_get_prev_next_page_name_penultimate():
     page_name = "status"
     result = pages.get_prev_next_page_name(page_name)
-    expected = ("metadata", None)
+    expected = ("metadata", "end")
+    assert result == expected, result
+
+
+def test_get_prev_next_page_name_last():
+    page_name = "end"
+    result = pages.get_prev_next_page_name(page_name)
+    expected = ("status", None)
     assert result == expected, result
