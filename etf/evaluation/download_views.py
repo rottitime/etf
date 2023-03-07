adc194037b656c2067f781872df0c0fc0fab8076
Nina Menezes <nina.menezes@cabinetoffice.gov.uk>
2023-03-07 16:15:51 +0000

    update views for data download

--- etf/evaluation/download_views.py
+++ etf/evaluation/download_views.py
@@ -4,6 +4,8 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import render
 from flatten_json import flatten
+from django.shortcuts import redirect, render
+from django.urls import reverse
 
 from etf.evaluation.models import Evaluation, EvaluationStatus
 from etf.evaluation.schemas import EvaluationSchema
@@ -11,6 +13,8 @@ from etf.evaluation.schemas import EvaluationSchema
 
 @login_required
 def download_json_view(request):
+    print(request.GET.get("my_evaluations"))
+    print(request.POST.dict())
     visible_qs = Evaluation.objects.exclude(status=EvaluationStatus.DRAFT)
     eval_schema = EvaluationSchema()
     data = eval_schema.dumps(visible_qs, many=True)
@@ -44,6 +48,35 @@ def download_csv_view(request):
 
 
 @login_required
+def download_data_view(request, include_my_evals, include_cs_only, include_public, type):
+    visible_statuses_to_include = []
+    if include_cs_only:
+        visible_statuses_to_include.append(EvaluationStatus.CIVIL_SERVICE)
+    elif include_public:
+        visible_statuses_to_include.append(EvaluationStatus.PUBLIC)
+    if include_my_evals:
+        user = request.user
+
+
+@login_required
 def download_page_view(request):
-    visible_qs = Evaluation.objects.exclude(status=EvaluationStatus.DRAFT)
-    return render(request, "data-download.html", {})
+    # TODO - errors, data?
+    type = None
+    if "json" in request.GET:
+        type = "json"
+    elif "csv" in request.GET:
+        type = "csv"
+
+    if not type:
+        return render(request, "data-download.html", {"errors": {}, "data": {}})
+
+    include_my_evals = False
+    include_cs = False
+    include_public = False
+    if "my_evaluations" in request.GET:
+        include_my_evals = True
+    if "civil_service_only" in request.GET:
+        include_cs = True
+    if "public" in request.GET:
+        include_public = True
+    return download_data_view(request, include_my_evals, include_cs, include_public, type)
