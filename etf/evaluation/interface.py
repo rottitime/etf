bc9f1ba9c120bb3cb73ab3278f4ae04cce0a2b00
Ed Singleton <singletoned@gmail.com>
2023-03-07 11:10:46 +0000

    Add update status to interface

--- etf/evaluation/interface.py
+++ etf/evaluation/interface.py
@@ -19,6 +19,13 @@ class UpdateEvaluationSchema(marshmallow.Schema):
     data = marshmallow.fields.Nested(schemas.EvaluationSchema)
 
 
+class UpdateEvaluationStatusSchema(marshmallow.Schema):
+    user_id = marshmallow.fields.UUID()
+    evaluation_id = marshmallow.fields.UUID()
+    page_name = marshmallow.fields.Str()
+    status = marshmallow.fields.Str()
+
+
 class Evaluation(Entity):
     @with_schema(load=CreateEvaluationSchema, dump=schemas.EvaluationSchema)
     @register_event("Evaluation created")
@@ -41,6 +48,13 @@ class Evaluation(Entity):
         evaluation = models.Evaluation.objects.get(id=evaluation_id, users__id=user_id)
         for key, value in data.items():
             setattr(evaluation, key, value)
+        evaluation.save()
+        return evaluation
+
+    @with_schema(load=UpdateEvaluationStatusSchema, dump=schemas.EvaluationSchema)
+    def update_page_status(self, user_id, evaluation_id, page_name, status):
+        evaluation = models.Evaluation.objects.get(id=evaluation_id, users__id=user_id)
+        evaluation.update_evaluation_page_status(page_name, status)
         evaluation.save()
         return evaluation
 
