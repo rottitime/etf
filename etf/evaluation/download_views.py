485c005a13419ee303f65072663f922633f129e0
Nina Menezes <nina.menezes@cabinetoffice.gov.uk>
2023-03-07 12:35:31 +0000

    add to csv download

--- etf/evaluation/download_views.py
+++ etf/evaluation/download_views.py
@@ -1,6 +1,9 @@
+import csv
+
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import render
+from flatten_json import flatten
 
 from etf.evaluation.models import Evaluation, EvaluationStatus
 from etf.evaluation.schemas import EvaluationSchema
@@ -23,21 +26,21 @@ def download_json_view(request):
 def download_csv_view(request):
     visible_qs = Evaluation.objects.exclude(status=EvaluationStatus.DRAFT)
     eval_schema = EvaluationSchema()
-    data = eval_schema.dumps(visible_qs, many=True)
+
+    data = eval_schema.dump(visible_qs, many=True)
+    flattened_dict = [flatten(d) for d in data]
+    # TODO - this doesn't get data in the format that we want it
+    # Also need headers for rows
+
     headers = {
         "Content-Type": "text/csv",
         "Content-Disposition": "attachment; filename=data.csv",
     }
-    response = HttpResponse(data, headers=headers)
+    response = HttpResponse(headers=headers)
+    writer = csv.writer(response)
+    for row in flattened_dict:
+        writer.writerow(row)
     return response
-
-
-# @login_required
-# def download_data_view(request, type, headers, include_civil_service=False):
-#     assert type in ["json"]  # TODO - add other types
-#     visible_qs = Evaluation.objects.exclude(status=EvaluationStatus.DRAFT)
-#     if not include_civil_service:
-#         visible_qs = visible_qs.filter(status=EvaluationStatus.PUBLIC)
 
 
 @login_required
