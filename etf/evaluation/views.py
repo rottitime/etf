5bd8af1145ebe54808150d77b555f7b1c7f58d2b
Elliot Moore <elliotmoorecornwall@outlook.com>
2023-03-10 08:23:15 +0000

    Missed comma

--- etf/evaluation/views.py
+++ etf/evaluation/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.postgres.search import (
     SearchQuery,
     SearchRank,
-    SearchVector
+    SearchVector,
 )
 from django.db.models import Q
 from django.http import HttpResponseNotAllowed
