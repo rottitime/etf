d790d5e065a181c4650d7e264fa85f84c080c6b6
Jas Aggarwal <1698584+rottitime@users.noreply.github.com>
2023-03-17 14:38:02 +0000

    working version of nestedcss

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,6 +1,12 @@
 import { defineConfig } from 'vite'
+import postcssNesting from 'postcss-nesting'
 
 export default defineConfig({
+  css: {
+    postcss: {
+      plugins: [postcssNesting],
+    },
+  },
   build: {
     outDir: '../static/dist',
     rollupOptions: {
