85df779c864df7d9431ff1785a690eb873ae30a4
Jas Aggarwal <1698584+rottitime@users.noreply.github.com>
2023-03-20 17:15:43 +0000

    accordion completed

--- web/src/main.ts
+++ web/src/main.ts
@@ -4,9 +4,54 @@ import './style/template.css'
 import './style/forms.css'
 import './style/components.css'
 
+//local development purposes only. to replace prod assets with dev
 if (import.meta.env.MODE === 'development') {
   ;['main-script', 'main-css'].forEach((id) => document.getElementById(id)?.remove())
 }
+
+const initAccordions = () => {
+  const accordionItems = document.querySelectorAll('.accordion li')
+
+  //remove all active classname
+  const removeAllActive = (el: Element) => {
+    el.parentNode
+      ?.querySelectorAll('.active')
+      .forEach((active) => active.classList.remove('active'))
+  }
+
+  //create wrapper
+  document.querySelectorAll('.accordion-content').forEach((content) => {
+    const wrapper = document.createElement('div')
+    wrapper.classList.add('accordion-content-wrapper')
+    wrapper?.addEventListener('transitionend', () => wrapper.setAttribute('style', ''))
+    content.parentNode?.insertBefore(wrapper, content)
+    wrapper.appendChild(content)
+  })
+
+  //click behaviour
+  accordionItems.forEach((accordion) => {
+    const button = accordion.querySelector('button')
+    const wrapper = accordion.querySelector(
+      '.accordion-content-wrapper'
+    ) as HTMLDivElement
+    const content = wrapper?.querySelector('.accordion-content')
+
+    button?.addEventListener('click', () => {
+      if (button.classList.contains('active')) {
+        removeAllActive(accordion)
+      } else {
+        const height = content?.clientHeight
+        removeAllActive(accordion)
+        button.classList.add('active')
+        wrapper?.setAttribute('style', `max-height:${height}px`)
+      }
+    })
+  })
+}
+
+window.addEventListener('load', () => {
+  initAccordions()
+})
 
 // import typescriptLogo from './typescript.svg'
 // import viteLogo from '/vite.svg'
