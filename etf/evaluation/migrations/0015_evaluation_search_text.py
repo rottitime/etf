f93382429c6bc080e13d5bff53cf63240e3f12ea
Elliot Moore <elliotmoorecornwall@outlook.com>
2023-03-21 16:30:27 +0000

    Formatted files

--- etf/evaluation/migrations/0015_evaluation_search_text.py
+++ etf/evaluation/migrations/0015_evaluation_search_text.py
@@ -4,15 +4,14 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
-        ('evaluation', '0014_auto_20230312_1710'),
+        ("evaluation", "0014_auto_20230312_1710"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='evaluation',
-            name='search_text',
+            model_name="evaluation",
+            name="search_text",
             field=models.TextField(blank=True, max_length=65536, null=True),
         ),
     ]
