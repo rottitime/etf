0e94414d4fbbc5f55f03e9c16efa4042989690ec
Elliot Moore <elliotmoorecornwall@outlook.com>
2023-03-21 16:01:13 +0000

    Added search text for evaluations as a combined field of all text fields from a specific evaluation entity and all related entities

--- etf/evaluation/models.py
+++ etf/evaluation/models.py
@@ -201,6 +201,9 @@ class Evaluation(TimeStampedModel, UUIDPrimaryKeyBase, NamedModel):
     other_eval_summary_findings = models.TextField(blank=True, null=True)
     other_eval_findings = models.TextField(blank=True, null=True)
 
+    # Search
+    search_text = models.TextField(blank=True, null=True, max_length=65536)
+
     # TODO - add fields on evaluation design, analysis and findings
 
     def update_evaluation_page_status(self, page_name, status):
@@ -237,6 +240,191 @@ class Evaluation(TimeStampedModel, UUIDPrimaryKeyBase, NamedModel):
     def __str__(self):
         return f"{self.id} : {self.title}"
 
+    def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
+        # TODO: reduce massive duplication in search text calculations
+        all_fields = self._meta.fields
+        combined_field_data = ""
+
+        # Unique fields
+        unique_fields = ["users"]
+
+        # List fields
+        list_fields = ["topics", "organisations"]
+
+        # Ignore fields
+        search_text_field = "search_text"
+        page_statuses_field = "page_statuses"
+
+        # Foreign key fields
+        foreign_key_fields = [
+            "interventions",
+            "outcome_measures",
+            "other_measures",
+            "process_standards",
+            "link_other_services",
+            "costs",
+            "documents",
+            "event_dates",
+        ]
+
+        # Multiple choice fields
+        multiple_choice_fields = ["evaluation_type", "economic_eval_type", "status", "impact_eval_design_name"]
+
+        # Single choice fields
+        single_choice_fields = [
+            "ethics_committee_approval",
+            "impact_eval_fidelity",
+            "impact_eval_framework",
+            "impact_eval_effect_measure_interval",
+            "impact_eval_basis",
+            "impact_eval_effect_measure_type",
+            "impact_eval_interpretation_type",
+            "impact_eval_interpretation",
+        ]
+
+        # Simple fields
+        exclusion_fields = (
+            foreign_key_fields
+            + multiple_choice_fields
+            + single_choice_fields
+            + [search_text_field]
+            + [page_statuses_field]
+            + list_fields
+            + unique_fields
+        )
+        simple_fields = [field for field in all_fields if field.name not in exclusion_fields]
+
+        for f in simple_fields:
+            value = self.__getattribute__(f.name)
+            if value:
+                combined_field_data += f"{value}|"
+        for foreign_key_field in foreign_key_fields:
+            related_field = self._meta.get_field(foreign_key_field)
+            relevant_model = related_field.related_model
+            related_objects = relevant_model.objects.filter(evaluation_id=self.id)
+
+            if related_objects:
+                for related_object in related_objects:
+                    related_object_search_text = related_object.get_search_text()
+                    if related_object_search_text:
+                        combined_field_data += f"{related_object_search_text}|"
+
+        if self.evaluation_type:
+            evaluation_types = [
+                value[1] for value in choices.EvaluationTypeOptions.choices if value[0] in self.evaluation_type
+            ]
+            if evaluation_types:
+                evaluation_type_text = "|".join(evaluation_types)
+                combined_field_data += f"{evaluation_type_text}|"
+
+        if self.economic_eval_type:
+            economic_eval_types = [
+                value[1] for value in choices.EconomicEvaluationType.choices if value[0] in self.economic_eval_type
+            ]
+            if economic_eval_types:
+                economic_eval_types_text = "|".join(economic_eval_types)
+                combined_field_data += f"{economic_eval_types_text}|"
+
+        if self.status:
+            status = [value[1] for value in choices.EvaluationStatus.choices if value[0] in self.status]
+            if status:
+                status_text = "|".join(status)
+                combined_field_data += f"{status_text}|"
+
+        if self.impact_eval_design_name:
+            impact_eval_design_name = [
+                value[1] for value in choices.ImpactEvalDesign.choices if value[0] in self.impact_eval_design_name
+            ]
+            if impact_eval_design_name:
+                impact_eval_design_name_text = "|".join(impact_eval_design_name)
+                combined_field_data += f"{impact_eval_design_name_text}|"
+
+        if self.ethics_committee_approval:
+            ethics_committee_approval = [
+                value[1] for value in choices.YesNo.choices if value[0] in self.ethics_committee_approval
+            ]
+            if ethics_committee_approval:
+                ethics_committee_approval_text = "|".join(ethics_committee_approval)
+                combined_field_data += f"{ethics_committee_approval_text}|"
+
+        if self.impact_eval_fidelity:
+            impact_eval_fidelity = [
+                value[1] for value in choices.YesNo.choices if value[0] in self.impact_eval_fidelity
+            ]
+            if impact_eval_fidelity:
+                impact_eval_fidelity_text = "|".join(impact_eval_fidelity)
+                combined_field_data += f"{impact_eval_fidelity_text}|"
+
+        if self.impact_eval_effect_measure_interval:
+            impact_eval_effect_measure_interval = [
+                value[1]
+                for value in choices.ImpactMeasureInterval.choices
+                if value[0] in self.impact_eval_effect_measure_interval
+            ]
+            if impact_eval_effect_measure_interval:
+                impact_eval_effect_measure_interval_text = "|".join(impact_eval_effect_measure_interval)
+                combined_field_data += f"{impact_eval_effect_measure_interval_text}|"
+
+        if self.impact_eval_framework:
+            impact_eval_framework = [
+                value[1] for value in choices.ImpactFramework.choices if value[0] in self.impact_eval_framework
+            ]
+            if impact_eval_framework:
+                impact_eval_framework_text = "|".join(impact_eval_framework)
+                combined_field_data += f"{impact_eval_framework_text}|"
+
+        if self.impact_eval_basis:
+            impact_eval_basis = [
+                value[1] for value in choices.ImpactAnalysisBasis.choices if value[0] in self.impact_eval_basis
+            ]
+            if impact_eval_basis:
+                impact_eval_basis_text = "|".join(impact_eval_basis)
+                combined_field_data += f"{impact_eval_basis_text}|"
+
+        if self.impact_eval_effect_measure_type:
+            impact_eval_effect_measure_type = [
+                value[1]
+                for value in choices.ImpactMeasureType.choices
+                if value[0] in self.impact_eval_effect_measure_type
+            ]
+            if impact_eval_effect_measure_type:
+                impact_eval_effect_measure_type_text = "|".join(impact_eval_effect_measure_type)
+                combined_field_data += f"{impact_eval_effect_measure_type_text}|"
+
+        if self.impact_eval_interpretation_type:
+            impact_eval_interpretation_type = [
+                value[1]
+                for value in choices.ImpactEvalInterpretation.choices
+                if value[0] in self.impact_eval_interpretation_type
+            ]
+            if impact_eval_interpretation_type:
+                impact_eval_interpretation_type_text = "|".join(impact_eval_interpretation_type)
+                combined_field_data += f"{impact_eval_interpretation_type_text}|"
+
+        if self.impact_eval_interpretation:
+            impact_eval_interpretation = [
+                value[1]
+                for value in choices.ImpactEvalInterpretation.choices
+                if value[0] in self.impact_eval_interpretation
+            ]
+            if impact_eval_interpretation:
+                impact_eval_interpretation_text = "|".join(impact_eval_interpretation)
+                combined_field_data += f"{impact_eval_interpretation_text}|"
+
+        if self.topics:
+            for topic in choices.Topic.choices:
+                if topic[0] in self.topics:
+                    combined_field_data += f"{topic[1]}|"
+
+            if self.organisations:
+                for organisation in enums.Organisation.choices:
+                    if organisation[0] in self.organisations:
+                        combined_field_data += f"{organisation[1]}|"
+
+        combined_field_data = combined_field_data.strip("|")
+        self.search_text = combined_field_data
+        return super().save()
+
 
 class Intervention(TimeStampedModel, UUIDPrimaryKeyBase, NamedModel):
     evaluation = models.ForeignKey(Evaluation, related_name="interventions", on_delete=models.CASCADE)
@@ -254,6 +442,26 @@ class Intervention(TimeStampedModel, UUIDPrimaryKeyBase, NamedModel):
     resource_requirements = models.TextField(blank=True, null=True)
     geographical_information = models.TextField(blank=True, null=True)
 
+    def get_search_text(self):
+        searchable_fields = [
+            str(self.name),
+            str(self.brief_description),
+            str(self.rationale),
+            str(self.materials_used),
+            str(self.procedures),
+            str(self.provider_description),
+            str(self.modes_of_delivery),
+            str(self.location),
+            str(self.frequency_of_delivery),
+            str(self.fidelity),
+            str(self.resource_requirements),
+            str(self.geographical_information),
+        ]
+
+        searchable_fields = [field for field in searchable_fields if field not in (None, "", " ", "None")]
+
+        return "|".join(searchable_fields)
+
 
 class OutcomeMeasure(TimeStampedModel, UUIDPrimaryKeyBase, NamedModel):
     evaluation = models.ForeignKey(Evaluation, related_name="outcome_measures", on_delete=models.CASCADE)
@@ -267,6 +475,31 @@ class OutcomeMeasure(TimeStampedModel, UUIDPrimaryKeyBase, NamedModel):
     minimum_difference = models.TextField(blank=True, null=True)
     relevance = models.TextField(blank=True, null=True)
 
+    def get_search_text(self):
+        primary_or_secondary = [
+            value[1] for value in choices.OutcomeType.choices if value[0] == self.primary_or_secondary
+        ]
+        direct_or_surrogate = [
+            value[1] for value in choices.OutcomeMeasure.choices if value[0] == self.direct_or_surrogate
+        ]
+        measure_type = [value[1] for value in choices.MeasureType.choices if value[0] == self.measure_type]
+
+        searchable_fields = [
+            str(self.name),
+            str(self.description),
+            str(self.collection_process),
+            str(self.timepoint),
+            str(self.minimum_difference),
+            str(self.relevance),
+            "|".join(primary_or_secondary),
+            "|".join(direct_or_surrogate),
+            "|".join(measure_type),
+        ]
+
+        searchable_fields = [field for field in searchable_fields if field not in (None, "", " ", "None")]
+
+        return "|".join(searchable_fields)
+
 
 class OtherMeasure(TimeStampedModel, UUIDPrimaryKeyBase, NamedModel):
     evaluation = models.ForeignKey(Evaluation, related_name="other_measures", on_delete=models.CASCADE)
@@ -275,12 +508,39 @@ class OtherMeasure(TimeStampedModel, UUIDPrimaryKeyBase, NamedModel):
     description = models.TextField(blank=True, null=True)
     collection_process = models.TextField(blank=True, null=True)
 
+    def get_search_text(self):
+        measure_type = [value[1] for value in choices.MeasureType.choices if value[0] == self.measure_type]
+
+        searchable_fields = [
+            str(self.name),
+            str(self.description),
+            str(self.collection_process),
+            "|".join(measure_type),
+        ]
+
+        searchable_fields = [field for field in searchable_fields if field not in (None, "", " ", "None")]
+
+        return "|".join(searchable_fields)
+
 
 class ProcessStandard(TimeStampedModel, UUIDPrimaryKeyBase, NamedModel):
     evaluation = models.ForeignKey(Evaluation, related_name="process_standards", on_delete=models.CASCADE)
     name = models.CharField(max_length=256)
     conformity = models.CharField(max_length=10, blank=True, null=True)
     description = models.TextField(blank=True, null=True)
+
+    def get_search_text(self):
+        conformity = [value[1] for value in choices.YesNoPartial.choices if value[0] in self.conformity]
+
+        searchable_fields = [
+            str(self.name),
+            str(self.description),
+            "|".join(conformity),
+        ]
+
+        searchable_fields = [field for field in searchable_fields if field not in (None, "", " ", "None")]
+
+        return "|".join(searchable_fields)
 
 
 class Document(TimeStampedModel, UUIDPrimaryKeyBase, NamedModel):
@@ -293,6 +553,20 @@ class Document(TimeStampedModel, UUIDPrimaryKeyBase, NamedModel):
 
     _name_field = "title"
 
+    def get_search_text(self):
+        document_types = [value[1] for value in choices.DocumentType.choices if value[0] in self.document_types]
+
+        searchable_fields = [
+            str(self.title),
+            str(self.description),
+            str(self.url),
+            "|".join(document_types),
+        ]
+
+        searchable_fields = [field for field in searchable_fields if field not in (None, "", " ", "None")]
+
+        return "|".join(searchable_fields)
+
 
 class EventDate(TimeStampedModel, UUIDPrimaryKeyBase, NamedModel):
     evaluation = models.ForeignKey(Evaluation, related_name="event_dates", on_delete=models.CASCADE)
@@ -303,6 +577,21 @@ class EventDate(TimeStampedModel, UUIDPrimaryKeyBase, NamedModel):
 
     _name_field = "event_date_name"
 
+    def get_search_text(self):
+        event_date_type = [value[1] for value in choices.EventDateType.choices if value[0] == self.event_date_type]
+        event_date_name = [value[1] for value in choices.EventDateOption.choices if value[0] == self.event_date_name]
+
+        searchable_fields = [
+            str(self.date),
+            str(self.reasons_for_change),
+            "|".join(event_date_type),
+            "|".join(event_date_name),
+        ]
+
+        searchable_fields = [field for field in searchable_fields if field not in (None, "", " ", "None", [])]
+
+        return "|".join(searchable_fields)
+
 
 class LinkOtherService(TimeStampedModel, UUIDPrimaryKeyBase, NamedModel):
     evaluation = models.ForeignKey(Evaluation, related_name="link_other_services", on_delete=models.CASCADE)
@@ -310,6 +599,16 @@ class LinkOtherService(TimeStampedModel, UUIDPrimaryKeyBase, NamedModel):
     link_or_identifier = models.CharField(max_length=256, blank=True, null=True)
 
     _name_field = "name_of_service"
+
+    def get_search_text(self):
+        searchable_fields = [
+            str(self.name_of_service),
+            str(self.link_or_identifier),
+        ]
+
+        searchable_fields = [field for field in searchable_fields if field not in (None, "", " ", "None")]
+
+        return "|".join(searchable_fields)
 
 
 class EvaluationCost(TimeStampedModel, UUIDPrimaryKeyBase, NamedModel):
@@ -323,3 +622,16 @@ class EvaluationCost(TimeStampedModel, UUIDPrimaryKeyBase, NamedModel):
     # TODO - add column for notes on evaluation costs
 
     _name_field = "item_name"
+
+    def get_search_text(self):
+        searchable_fields = [
+            str(self.item_name),
+            str(self.description),
+            str(self.item_cost),
+            str(self.earliest_spend_date),
+            str(self.latest_spend_date),
+        ]
+
+        searchable_fields = [field for field in searchable_fields if field not in (None, "", " ", "None")]
+
+        return "|".join(searchable_fields)
