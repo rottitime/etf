b5a0c3e89da54c2acd78dc9fdb7fc20004ee301d
Nina Menezes <nina.menezes@cabinetoffice.gov.uk>
2023-03-23 14:10:34 +0000

    add new function to convert to df

--- scripts/upload_rsm_data.py
+++ scripts/upload_rsm_data.py
@@ -5,12 +5,14 @@ RSM_FILENAME = "test.xlsx"
 
 # Assumptions 
 # Sheets with relevant data to import are precisely the ones whose names are integers
+# Assume columns and titles
 
-def get_all_data():
+def get_all_import_data():
     data = pd.read_excel(RSM_FILENAME, sheet_name=None, header=[0,1])
     sheet_names = list(data.keys())
     relevant_sheet_names = [i for i in sheet_names if i.isdigit()]
-    return relevant_sheet_names, data
+    relevant_data = {n: df for n, df in data if n in relevant_sheet_names}
+    return relevant_data
 
 
 def tidy_column_titles(df):
@@ -18,9 +20,18 @@ def tidy_column_titles(df):
     df.columns = ["_".join(col).strip() for col in df.columns.values]
     df.columns = [col.replace(" ", "_") for col in df.columns.values]
     df.columns = [col.lower() for col in df.columns.values]
+    # TODO - assert number of columns
+    # TODO - assert columns names
     return df
 
 
+def get_all_import_data_df():
+    data = get_all_import_data()
+    all_dfs = list(data.values())
+    transformed_dfs = [tidy_column_titles(df) for df in all_dfs]
+    df = pd.concat(transformed_dfs)
+    return df
+    
 
 
 
