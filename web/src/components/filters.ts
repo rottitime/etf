d117e66c24b57ed744be9a14142f2bfe4485c30e
Jas Aggarwal <1698584+rottitime@users.noreply.github.com>
2023-03-24 11:44:26 +0000

    button reset

--- web/src/components/filters.ts
+++ web/src/components/filters.ts
@@ -3,17 +3,31 @@ import '../style/filters.css'
 const setTotal = (filterControl: HTMLElement, name: string) => {
   const total = filterControl.querySelectorAll(`[name=${name}]:checked`).length
 
-  console.log({ total, name })
-
   document
     .querySelectorAll(`.filter-control-total[data-name=${name}]`)
     .forEach((control) => {
-      console.log({ control })
       control.innerHTML = total.toString()
     })
 }
 
 const setupFilters = () => {
+  document.querySelectorAll<HTMLElement>('.filter-control').forEach((filterControl) => {
+    filterControl.querySelectorAll('button[type="reset"]').forEach((button) => {
+      button.addEventListener('click', () => {
+        const checkboxes = filterControl.querySelectorAll<HTMLInputElement>(
+          'input[type=checkbox][name]:checked'
+        )
+
+        checkboxes.forEach((checkbox) => {
+          checkbox.removeAttribute('checked')
+          checkbox.checked = false
+        })
+
+        !!checkboxes.length && setTotal(filterControl, checkboxes[0].name)
+      })
+    })
+  })
+
   document
     .querySelectorAll('.filter-control input[type="checkbox"]')
     .forEach((checkbox) => {
