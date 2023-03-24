a3b7fc14477f8ccce868643ea7148cf99fe7e2ac
Nina Menezes <nina.menezes@cabinetoffice.gov.uk>
2023-03-24 22:56:31 +0000

    update tests

--- tests/test_models.py
+++ tests/test_models.py
@@ -73,8 +73,9 @@ def test_search_text():
     test_eval.save()
     outcome_measure = models.OutcomeMeasure(evaluation=test_eval, name="My new outcome measure")
     outcome_measure.save()
+    test_eval.save() # TODO - need to change save for related objects
     search_text = test_eval.search_text
     assert "elephants" in search_text, search_text
     assert "DfE" in search_text, search_text
-    assert "My new outcome measure" in search_text
-    assert "Cost-benefit analysis" in search_text
+    assert "My new outcome measure" in search_text, search_text
+    assert "Cost-benefit analysis" in search_text, search_text
