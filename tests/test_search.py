2cd5cd08e727b15a07dd242a02cf8d2079a783e3
Nina Menezes <nina.menezes@cabinetoffice.gov.uk>
2023-03-24 17:54:47 +0000

    update search

--- tests/test_search.py
+++ tests/test_search.py
@@ -1,18 +1,27 @@
-# from . import utils
-#
-# USER_DATA = {"email": "mr_search_test@example.com", "password": "1-h4t3-p455w0rd-c0mpl3xity-53tt1ng5"}
-#
-#
+from . import utils
+from etf.evaluation import models
+
+USER_DATA = {"email": "mr_search_test@example.com", "password": "1-h4t3-p455w0rd-c0mpl3xity-53tt1ng5"}
+
+
 # def test_search():
 #     client = utils.make_testino_client()
 #     utils.register(client, **USER_DATA)
-#
+
 #     page = client.get("/")
 #     assert page.status_code == 200, page.status_code
 #     assert page.has_text("My evaluations")
-#
+
 #     form = page.get_form("#search-form")
 #     form["search_phrase"] = "Floop"
 #     page = form.submit()
-#
+
 #     assert page.has_text("0 evaluations found")
+
+
+
+# def test_search_text():
+#     test_eval = models.Evaluation(title="Test search eval", brief_description="Dancing elephants")
+#     test_eval.save()
+#     outcome_measure = models.OutcomeMeasure(evaluation=test_eval, name="My new outcome measure")
+#     outcome_measure.save()
