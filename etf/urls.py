974fa2e9148bb34c7ee56af1498e871933d320b2
Elliot Moore <elliotmoorecornwall@outlook.com>
2023-03-28 12:53:37 +0100

    Fixed sorting

--- etf/urls.py
+++ etf/urls.py
@@ -2,13 +2,8 @@ from django.contrib import admin
 from django.shortcuts import redirect
 from django.urls import include, path
 
-from etf.evaluation import (
-    authentication_views,
-    download_views,
-    submission_views,
-    views,
-    overview_views,
-)
+from etf.evaluation import (authentication_views, download_views,
+                            overview_views, submission_views, views)
 
 urlpatterns = [
     path("", submission_views.index_view, name="index"),
