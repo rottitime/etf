fdb3b1bd67e35859e151b20267b1c23a227e8c99
Elliot Moore <elliotmoorecornwall@outlook.com>
2023-04-10 11:52:28 +0100

    Altered contributor view to no longer support htmx as the view is now a simple single page

--- etf/evaluation/views.py
+++ etf/evaluation/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import Group
 from django.contrib.postgres.search import (
     SearchQuery,
     SearchRank,
