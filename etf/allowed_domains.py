50b3ff28a6263b59fa735e9c8b74d9e26f26fe6f
Nina Menezes <nina.menezes@cabinetoffice.gov.uk>
2023-04-11 21:42:40 +0100

    update domains

--- etf/allowed_domains.py
+++ etf/allowed_domains.py
@@ -30,47 +30,48 @@ CABINET_OFFICE = frozenset(
         "odandd.gov.uk",
     ]
 )
-DEPT_FOR_BUSINESS_AND_TRADE = frozenset([])
-DEPT_FOR_CULTURE_MEDIA_SPORT = frozenset([])
-DEPT_FOR_EDUCATION = frozenset([])
-DEPT_FOR_ENERGY_SECURITY_NET_ZERO = frozenset([])
-DEFRA = frozenset([])
-DLUHC = frozenset([])
-DEPT_SCIENCE_INNOVATION_TECHNOLOGY = frozenset([])
-DEPT_TRANSPORT = frozenset([])
-DEPT_WORK_PENSIONS = frozenset([])
-DEPT_HEALTH_SOCIAL_CARE = frozenset([])
-FOREIGN_OFFICE = frozenset([])
-HMT = frozenset([])
-HOME_OFFICE = frozenset([])
-MINISTRY_OF_DEFENCE = frozenset([])
-MINISTRY_OF_JUSTICE = frozenset([])
+DEPT_FOR_BUSINESS_AND_TRADE = frozenset(["beis.gov.uk", "trade.gov.uk", "companieshouse.gov.uk"])
+DEPT_FOR_CULTURE_MEDIA_SPORT = frozenset(["dcms.gov.uk"])
+DEPT_FOR_EDUCATION = frozenset(["education.gov.uk"])
+DEPT_FOR_ENERGY_SECURITY_NET_ZERO = frozenset(["beis.gov.uk"])
+DEFRA = frozenset(["defra.gov.uk"])
+DLUHC = frozenset(["levellingup.gov.uk"])
+DEPT_SCIENCE_INNOVATION_TECHNOLOGY = frozenset(["beis.gov.uk"])
+DEPT_TRANSPORT = frozenset(["dft.gov.uk"])
+DEPT_WORK_PENSIONS = frozenset(["dwp.gov.uk"])
+DEPT_HEALTH_SOCIAL_CARE = frozenset(["dhsc.gov.uk"])
+FOREIGN_OFFICE = frozenset(["fcdo.gov.uk"])
+HMT = frozenset(["hmtreasury.gov.uk"])
+HOME_OFFICE = frozenset(["homeoffice.gov.uk"])
+MINISTRY_OF_DEFENCE = frozenset(["mod.gov.uk", "dstl.gov.uk"])
+MINISTRY_OF_JUSTICE = frozenset(["justice.gov.uk", "digital.justice.gov.uk"])
 NI_OFFICE = frozenset([])
 OFFICE_OF_ADVOCATE_GENERAL_FOR_SCOTLAND = frozenset([])
 OFFICE_OF_LEADER_OF_HOUSE_OF_COMMONS = frozenset([])
 OFFICE_OF_LEADER_HOUSE_OF_LORDS = frozenset([])
 OFFICE_OF_SOS_SCOTLAND = frozenset([])
 OFFICE_OF_SOS_WALES = frozenset([])
-UK_EXPORT_FINANCE = frozenset([])
+UK_EXPORT_FINANCE = frozenset(["ukexportfinance.gov.uk"])
+
 
 # Non-ministerial departments
 
 CHARITY_COMMISSION = frozenset([])
-COMPETITION_MARKETS_AUTHORITY = frozenset([])
+COMPETITION_MARKETS_AUTHORITY = frozenset(["cma.gov.uk"])
 CROWN_PROSECUTION_SERVICE = frozenset([])
 FOOD_STANDARDS_AGENCY = frozenset([])
 FORESTRY_COMMISSION = frozenset([])
 GOV_ACTUARY_DEPT = frozenset([])
 GOV_LEGAL_DEPT = frozenset([])
 HM_LAND_REGISTRY = frozenset([])
-HMRC = frozenset([])
+HMRC = frozenset(["hmrc.gov.uk"])
 NS_AND_I = frozenset([])
 NATIONAL_ARCHIVES = frozenset([])
-NATIONAL_CRIME_AGENCY = frozenset([])
+NATIONAL_CRIME_AGENCY = frozenset(["nca.gov.uk"])
 OFFICE_OF_RAIL_AND_ROAD = frozenset([])
 OFGEM = frozenset([])
 OFQUAL = frozenset([])
-OFSTED = frozenset([])
+OFSTED = frozenset(["ofsted.gov.uk"])
 SERIOUS_FRAUD_OFFICE = frozenset([])
 SUPREME_COURT_UK = frozenset([])
 UK_STATISTICS_AUTHORITY = frozenset([])
@@ -78,9 +79,14 @@ WATER_SERVICES_REGULATION_AUTHORITY = frozenset([])
 
 # Devolved administrations
 
-SCOTTISH_GOVERNMENT = frozenset([])
+SCOTTISH_GOVERNMENT = frozenset(["gov.scot"])
 WELSH_GOVERNMENT = frozenset([])
 NI_GOVERNMENT = frozenset([])  # Note, this is not part of UK Civil Service
+
+# Other emails
+# Things like profession emails, Fast Stream emails etc
+
+OTHER = frozenset(["faststream.civilservice.gov.uk"])
 
 
 # All domains
@@ -132,4 +138,5 @@ CIVIL_SERVICE_DOMAINS = frozenset.union(
     SCOTTISH_GOVERNMENT,
     WELSH_GOVERNMENT,
     NI_GOVERNMENT,
+    OTHER,
 )
