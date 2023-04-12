87cd4ab9ebe656df07efcee9431f646dc4707739
Jas Aggarwal <1698584+rottitime@users.noreply.github.com>
2023-04-12 13:02:07 +0100

    live reload for html files

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -2,12 +2,17 @@ import { defineConfig } from 'vite'
 import postcssNesting from 'postcss-nesting'
 import postcssCustomMedia from 'postcss-custom-media'
 import svgLoader from 'vite-svg-loader'
+// eslint-disable-next-line import/no-named-as-default
+import liveReload from 'vite-plugin-live-reload'
 
 export default defineConfig({
   server: {
     port: 5173
   },
-  plugins: [svgLoader({ defaultImport: 'raw' })],
+  plugins: [
+    svgLoader({ defaultImport: 'raw' }),
+    liveReload('../etf/templates/**/*.*', { alwaysReload: true })
+  ],
   css: {
     postcss: {
       plugins: [postcssNesting, postcssCustomMedia]
