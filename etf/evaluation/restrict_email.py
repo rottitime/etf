6cf658978ee1ef84e6a70b5507722aafa64266fb
Nina Menezes <nina.menezes@cabinetoffice.gov.uk>
2023-04-12 09:54:20 +0100

    remove unnecessary check

--- etf/evaluation/restrict_email.py
+++ etf/evaluation/restrict_email.py
@@ -4,10 +4,8 @@ from django.forms import ValidationError
 
 def clean_email(email):
     email = email.lower()
-    email_allowed = False
-    if not email_allowed:
-        domain = email.split("@")[-1]
-        email_allowed = domain in settings.ALLOWED_DOMAINS
+    domain = email.split("@")[-1]
+    email_allowed = domain in settings.ALLOWED_DOMAINS
     if not email_allowed:
         raise ValidationError(
             "This email domain is not yet supported. Please contact the site admin team if you think this is incorrect."
