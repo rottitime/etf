18a5f06061b55304c9f96559a17011c889e3627a
Ed Singleton <singletoned@gmail.com>
2023-04-19 13:01:45 +0100

    Add test for authenticated pages

--- tests/test_authenticated_pages.py
+++ tests/test_authenticated_pages.py
@@ -1,10 +1,9 @@
 from etf.evaluation import fields, models
 
 from . import utils
-from .utils import with_authenticated_client
 
 
-@with_authenticated_client
+@utils.with_authenticated_client
 def test_get_pages_logged_in(client):
     urls_to_test = ["/search/", "/my-evaluations/"]
     for url in urls_to_test:
@@ -28,3 +27,46 @@ def test_malformed_markdown():
     ] = "Please provide one or two sentences to describe the evaluation."
     response = client.get(f"evaluation/{evaluation.id}/description/")
     assert response.has_text("Please provide one or two sentences to describe the evaluation.")
+
+
+@utils.with_authenticated_client
+def test_evaluation_permissions(client):
+    # Setup evaluation
+    test_email = "test-evaluation-permissions@example.com"
+    user, _ = models.User.objects.get_or_create(email=test_email)
+    evaluation = models.Evaluation.objects.create(title="Test evaluation")
+    evaluation.save()
+    user.evaluations.add(evaluation)
+    user.save()
+
+    urls = (
+        f"/evaluation/{evaluation.id}/",
+        f"evaluation/{evaluation.id}/title/",
+        f"evaluation/{evaluation.id}/description/",
+        f"evaluation/{evaluation.id}/issue-description/",
+        f"evaluation/{evaluation.id}/studied-population/",
+        f"evaluation/{evaluation.id}/participant-recruitment/",
+        f"evaluation/{evaluation.id}/policy-costs/",
+        f"evaluation/{evaluation.id}/publication-intention/",
+        f"evaluation/{evaluation.id}/evaluation-types/",
+        f"evaluation/{evaluation.id}/impact-design/",
+        f"evaluation/{evaluation.id}/impact-analysis/",
+        f"evaluation/{evaluation.id}/process-design/",
+        f"evaluation/{evaluation.id}/process-analysis/",
+        f"evaluation/{evaluation.id}/economic-design/",
+        f"evaluation/{evaluation.id}/economic-analysis/",
+        f"evaluation/{evaluation.id}/other-design/",
+        f"evaluation/{evaluation.id}/other-analysis/",
+        f"evaluation/{evaluation.id}/ethics/",
+        f"evaluation/{evaluation.id}/impact-findings/",
+        f"evaluation/{evaluation.id}/economic-findings/",
+        f"evaluation/{evaluation.id}/process-findings/",
+        f"evaluation/{evaluation.id}/other-findings/",
+        f"evaluation/{evaluation.id}/metadata/",
+        f"evaluation/{evaluation.id}/status/",
+        f"evaluation/{evaluation.id}/end/",
+    )
+
+    for url in urls:
+        response = client.get(url)
+        assert response.status_code == 404
