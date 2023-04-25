0dd637dc06e5c3ffddd1ecbcb131f55cf0a57bea
Elliot Moore <elliotmoorecornwall@outlook.com>
2023-04-25 18:43:06 +0100

    formatting

--- etf/evaluation/context_processors.py
+++ etf/evaluation/context_processors.py
@@ -2,4 +2,6 @@ from django.conf import settings
 
 
 def space_name(request):
-    return {'space_name': settings.VCAP_APPLICATION.get("space_name", "unknown"),}
+    return {
+        "space_name": settings.VCAP_APPLICATION.get("space_name", "unknown"),
+    }
