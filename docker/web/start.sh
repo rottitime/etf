1aa7b60259c9c601acd141e4375d9618322142e6
rickyp72 <2026055+rickyp72@users.noreply.github.com>
2023-04-26 10:47:25 +0100

    copy css files to local volume in startup script

--- docker/web/start.sh
+++ docker/web/start.sh
@@ -3,5 +3,8 @@
 set -o errexit
 set -o nounset
 
+# Copy across generated files to local docker-compose volume
+cp -R /output/* /app/static/dist/
+
 python manage.py migrate --noinput
 watchmedo auto-restart --directory=./  --pattern=""*.py"" --recursive -- waitress-serve --port=$PORT --threads=8 etf.wsgi:application
