4ca87243a64eb9303fb5de4891e476a7b4ee1287
Jas Aggarwal <1698584+rottitime@users.noreply.github.com>
2023-05-02 12:30:59 +0100

    validation bugfixes: meta tags, viewport, div elements within buttons

--- web/src/components/icon.ts
+++ web/src/components/icon.ts
@@ -2,7 +2,7 @@ const setupIcons = () => {
   const prefix = 'icon-'
   document.querySelectorAll(`i[class^="${prefix}"]`).forEach(async (e) => {
     const iconName = e.className.replace(prefix, '')
-    const html = await import(`../svg/${iconName}.svg`)
+    const html = await import(`../svg/${iconName}.svg?ddede`)
     e.outerHTML = html.default
   })
 }
