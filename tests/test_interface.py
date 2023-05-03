a544ef571dab81b906bed66472fd29e72574a4cf
Nina Menezes <nina.menezes@cabinetoffice.gov.uk>
2023-05-03 11:15:00 +0100

    update test for interfaces

--- tests/test_interface.py
+++ tests/test_interface.py
@@ -11,10 +11,22 @@ def test_evaluation_facade():
 
     evaluation_id = result["id"]
     result = interface.facade.evaluation.get(user_id=user.id, evaluation_id=evaluation_id)
-
     assert result["users"] == expected
 
     data = {"title": "Flibble", "monetisation_approaches": "Sell, sell, sell"}
     result = interface.facade.evaluation.update(user_id=user.id, evaluation_id=evaluation_id, data=data)
     assert result["title"] == "Flibble"
     assert result["monetisation_approaches"] == "Sell, sell, sell"
+
+    data = {"evaluation_id": evaluation_id, "user_data": {"email": "new_user@example.com", "is_external_user": False}}
+    result = interface.facade.evaluation.add_user_to_evaluation(data)
+    assert result["user_created"], result
+    assert result["evaluation_id"] == evaluation_id, result
+
+    data = {
+        "evaluation_id": evaluation_id,
+        "user_data": {"email": "mr_interface_test@example.com", "is_external_user": False},
+    }
+    result = interface.facade.evaluation.add_user_to_evaluation(data)
+    assert not result["user_created"], result
+    assert result["evaluation_id"] == evaluation_id, result
