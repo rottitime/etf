bb0f5cecdcb04ad2bf036adc0743031fd0ed386f
Elliot Moore <elliotmoorecornwall@outlook.com>
2023-05-12 13:56:42 +0100

    Sort session middleware

--- etf/evaluation/session_middleware.py
+++ etf/evaluation/session_middleware.py
@@ -1,4 +1,4 @@
-from datetime import timedelta, datetime
+from datetime import datetime, timedelta
 
 from django.conf import settings
 from django.contrib.sessions.middleware import SessionMiddleware
