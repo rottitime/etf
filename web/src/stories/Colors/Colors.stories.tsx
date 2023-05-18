d327dd67f96cbae147fbef7670c36b79974af392
Jas Aggarwal <1698584+rottitime@users.noreply.github.com>
2023-05-18 17:02:09 +0100

    Tidy

--- web/src/stories/Colors/Colors.stories.tsx
+++ web/src/stories/Colors/Colors.stories.tsx
@@ -18,13 +18,16 @@ const palette = [
 
 const meta = {
   title: 'ETF/Colors',
-  tags: ['autodocs'],
   render: () =>
-    `<div class="color-wrapper">${palette.map(
-      (color) =>
-        `<div class="color-block" style="background-color:var(${color})">${color}</div>`
-    )}</div>`,
+    `<div class="color-wrapper">${palette
+      .map(
+        (color) =>
+          `<div class="color-block" style="background-color:var(${color})">${color}</div>`
+      )
+      .join('')}</div>`,
+
   parameters: {
+    controls: { hideNoControlsWarning: true },
     design: {
       type: 'figma',
       url: 'https://www.figma.com/file/pN3VtobnXOlTUDK4aiZa94/i-AI-DS?type=design&node-id=1-386&t=NMzWF77GnAa7BvPp-0'
