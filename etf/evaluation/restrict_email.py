a8d93811349be4d08951d76f62d2c2409580d58b
Melissa Ababio-Marsh <melissa.karikari-ababio@cabinetoffice.gov.uk>
2023-05-19 09:46:13 +0100

    Updated domain support message

--- etf/evaluation/restrict_email.py
+++ etf/evaluation/restrict_email.py
@@ -2,12 +2,11 @@ from django.conf import settings
 from django.forms import ValidationError
 
 
+
 def clean_email(email):
     email = email.lower()
     domain = email.split("@")[-1]
     email_allowed = domain in settings.ALLOWED_CIVIL_SERVICE_DOMAINS
     if not email_allowed:
-        raise ValidationError(
-            "This email domain is not yet supported. Please contact the site admin team if you think this is incorrect."
-        )
+        raise ValidationError (f"This email domain is not yet supported. Please contact the site admin team if you think this is incorrect <a href='mailto:{settings.FEEDBACK_EMAIL}'>{settings.FEEDBACK_EMAIL}</a>")
     return email
