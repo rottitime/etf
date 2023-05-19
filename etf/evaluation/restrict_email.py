57dbb5041bddcab097595e8c986255b840657133
Melissa Ababio-Marsh <melissa.karikari-ababio@cabinetoffice.gov.uk>
2023-05-19 14:06:37 +0100

    Updated text

--- etf/evaluation/restrict_email.py
+++ etf/evaluation/restrict_email.py
@@ -8,5 +8,5 @@ def clean_email(email):
     domain = email.split("@")[-1]
     email_allowed = domain in settings.ALLOWED_CIVIL_SERVICE_DOMAINS
     if not email_allowed:
-        raise ValidationError (f"This email domain is not yet supported. Please contact the site admin team if you think this is incorrect <a href='mailto:{settings.FEEDBACK_EMAIL}'>{settings.FEEDBACK_EMAIL}</a>")
+        raise ValidationError (f"Currently you need a civil service email addresses to register. If you think your email address should be allowed contact us on <a href='mailto:{settings.FEEDBACK_EMAIL}'>{settings.FEEDBACK_EMAIL}</a>")
     return email
