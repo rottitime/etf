6d5845fc95dc356da24202c612410461d87f2724
Nina Menezes <nina.menezes@cabinetoffice.gov.uk>
2023-05-19 14:16:30 +0100

    fix other typo

--- etf/evaluation/restrict_email.py
+++ etf/evaluation/restrict_email.py
@@ -2,11 +2,12 @@ from django.conf import settings
 from django.forms import ValidationError
 
 
-
 def clean_email(email):
     email = email.lower()
     domain = email.split("@")[-1]
     email_allowed = domain in settings.ALLOWED_CIVIL_SERVICE_DOMAINS
     if not email_allowed:
-        raise ValidationError (f"Currently you need a Civil Service email addresses to register. If you think your email address should be allowed contact us on <a href='mailto:{settings.FEEDBACK_EMAIL}'>{settings.FEEDBACK_EMAIL}</a>")
+        raise ValidationError(
+            f"Currently you need a Civil Service email address to register. If you think your email address should be allowed contact us on <a href='mailto:{settings.FEEDBACK_EMAIL}'>{settings.FEEDBACK_EMAIL}</a>"
+        )
     return email
