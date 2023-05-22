6b65430f776b536ad0f1add69806593b17609fae
Nina Menezes <nina.menezes@cabinetoffice.gov.uk>
2023-05-22 22:58:08 +0100

    update access tests

--- tests/test_access_evaluations.py
+++ tests/test_access_evaluations.py
@@ -7,22 +7,6 @@ from etf.evaluation import models
 from . import utils
 
 
-# TODO - do this better
-NAMES_TO_IGNORE = [
-    "intervention-page",
-    "outcome-measure-page",
-    "other-measure-page",
-    "processes-standard-page",
-    "evaluation-cost-page",
-    "document-page",
-    "link-page",
-    "event-date-page",
-    "grant-page",
-    "evaluation-contributor-remove",
-    "create-evaluation",
-    "evaluation-overview",
-]
-
 # Explicitly excluding URLs, to ensure that we don't miss out new URLs
 EDIT_OR_VIEW_EVALUATION_URL_PATTERNS = (
     set(urls.urlpatterns)
@@ -42,25 +26,61 @@ VIEW_EVALUATION_URL_PATTERNS = (
 )
 
 
-def get_url_for_evaluation(client, url_pattern, evaluation_id):
-    name = url_pattern.name
-    if name in NAMES_TO_IGNORE:
+# Non-standard URLs - explicitly write tests
+NAMES_TO_IGNORE = [
+    "evaluation-contributor-remove",
+    "create-evaluation",
+    "evaluation-overview",
+]
+
+
+RELATED_OBJECTS = {
+    "intervention-page": "Intervention",
+    "outcome-measure-page": "OutcomeMeasure",
+    "other-measure-page": "OtherMeasure",
+    "processes-standard-page": "ProcessStandard",
+    "evaluation-cost-page": "EvaluationCost",
+    "document-page": "Document",
+    "link-page": "LinkOtherService",
+    "event-date-page": "EventDate",
+    "grant-page": "Grant",
+}
+
+
+def get_url_for_evaluation_and_related_object(client, url_name, evaluation_id, related_id=None):
+    if (url_name in NAMES_TO_IGNORE) or (url_name in RELATED_OBJECTS.keys()):
         return
-    url = reverse(url_pattern.name, args=(evaluation_id,))
+    if related_id:
+        url = reverse(url_name, args=(evaluation_id, related_id))
+    else:
+        url = reverse(url_name, args=(evaluation_id,))
     response = client.get(url)
     return response
 
 
 @with_setup(utils.create_fake_evaluations, utils.remove_fake_evaluations)
 @utils.with_authenticated_client
-def test_edit_evaluations(client):
+def test_edit_or_view_evaluations(client):
     evaluation_not_editable = models.Evaluation.objects.filter(title="Draft evaluation 1").first()
-    evaluation_editable = models.Evaluation.objects.filter(title="Draft evaluation 2").first()
-    # for url_pattern in EDIT_OR_VIEW_EVALUATION_URL_PATTERNS:
-    #     response = get_url_for_evaluation(client, url_pattern, evaluation_editable.id)
-    #     if response:
-    #         assert response.status_code == 200
     for url_pattern in EDIT_OR_VIEW_EVALUATION_URL_PATTERNS:
-        response = get_url_for_evaluation(client, url_pattern, evaluation_not_editable.id)
+        response = get_url_for_evaluation_and_related_object(client, url_pattern.name, evaluation_not_editable.id)
         if response:
-            assert response.status_code == 404
+            assert response.status_code == 404, response.status_code
+
+
+# TODO - test edit only
+
+# TODO - test can view but not edit some
+
+
+@with_setup(utils.create_fake_evaluations, utils.remove_fake_evaluations)
+@utils.with_authenticated_client
+def test_edit_or_view_related_objects(client):
+    evaluation_not_editable = models.Evaluation.objects.filter(title="Draft evaluation 1").first()
+    for url_name, model_name in RELATED_OBJECTS.items():
+        model = getattr(models, model_name)
+        new_obj = model(evaluation__id=evaluation_not_editable.id)
+        new_obj.save()
+        related_id = new_obj.id
+        response = get_url_for_evaluation_and_related_object(client, url_name, evaluation_not_editable.id, related_id)
+        assert response.status_code == 404, response.status_code
