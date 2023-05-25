3d16a4f420fc4366405c317ac620aea03b033257
Elliot Moore <elliotmoorecornwall@outlook.com>
2023-05-25 11:49:16 +0100

    Missed options page in overview, incorrect page name in optional pages map

--- etf/evaluation/pages.py
+++ etf/evaluation/pages.py
@@ -56,6 +56,7 @@ section_pages = {
     "general": (
         "intro",
         "title",
+        "options",
         "description",
         "event-dates",
         "evaluation-types",
@@ -145,7 +146,7 @@ evaluation_type_page_mapping = {
 }
 
 page_options_mapping = {
-    "issue_description_option": "issue_description",
+    "issue_description_option": "issue-description",
     "ethics_option": "ethics",
     "grants_option": "grants",
 }
