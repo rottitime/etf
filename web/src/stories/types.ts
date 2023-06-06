119c28df5d3b353b9c6ba3a9b8a9602239c60ce1
Jas Aggarwal <1698584+rottitime@users.noreply.github.com>
2023-06-06 11:25:16 +0100

    new 'small' & 'accordion' attribute. Cleanup of code

--- web/src/stories/types.ts
+++ web/src/stories/types.ts
@@ -1,5 +1,14 @@
 export type Link = { href: string; text: string }
 
+export type Card = {
+  title: string
+  content: string
+  actions: boolean
+  small: boolean
+  accordion: boolean
+  onClick: () => void
+}
+
 export type FieldMeta = {
   label?: string
   helperText?: string
