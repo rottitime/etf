f89820da96675f412b9cee3e014ffbb0512cfdf0
Nina Menezes <nina.menezes@cabinetoffice.gov.uk>
2023-06-10 21:05:02 +0100

    tidy

--- tests/test_access_evaluations.py
+++ tests/test_access_evaluations.py
@@ -108,7 +108,6 @@ def test_view_not_edit_evaluations(client):
         for url_pattern in VIEW_EVALUATION_URL_PATTERNS:
             response = get_url_for_evaluation_and_related_object(client, url_pattern.name, evaluation.id)
             if response:
-                print(url_pattern.name)
                 assert response.status_code == 200, response.status_code
 
 
