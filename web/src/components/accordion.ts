3aa2a1baee169700f0abbf6ba8c2540c7a854081
Jas Aggarwal <1698584+rottitime@users.noreply.github.com>
2023-06-15 11:21:18 +0100

    accordion aria labels

--- web/src/components/accordion.ts
+++ web/src/components/accordion.ts
@@ -1,15 +1,42 @@
 class Accordion extends HTMLDivElement {
+  buttonId: string
+  sectionId: string
+
   constructor() {
     super()
+
+    const id = crypto.randomUUID()
+    this.buttonId = `btn-${id}`
+    this.sectionId = `pnl-${id}`
   }
 
   connectedCallback() {
     setTimeout(() => this.setup())
   }
 
+  private setupPanels() {
+    this.querySelectorAll('.accordion-panel').forEach((panel, i) => {
+      panel.setAttribute('role', 'region')
+      panel.setAttribute('aria-labelledby', this.buttonId + `-${i}`)
+      panel.id = this.sectionId + `-${i}`
+    })
+  }
+
   private setupButtons() {
-    this.querySelectorAll('.accordion-title').forEach((button) => {
+    this.querySelectorAll('.accordion-title').forEach((button, i) => {
       button.setAttribute('role', 'button')
+      button.id = this.buttonId + `-${i}`
+      button.setAttribute(
+        'aria-expanded',
+        String(button.getAttribute('aria-expanded') === 'true' || false)
+      )
+      button.setAttribute('aria-controls', this.sectionId + `-${i}`)
+
+      //event to toggle aria-expanded
+      button.addEventListener('click', () => {
+        const expanded = button.getAttribute('aria-expanded') === 'true' || false
+        button.setAttribute('aria-expanded', String(!expanded))
+      })
     })
   }
 
@@ -24,6 +51,7 @@ class Accordion extends HTMLDivElement {
 
   private setup() {
     this.setupButtons()
+    this.setupPanels()
     const showAll = this.createShowAll()
 
     this.prepend(showAll)
