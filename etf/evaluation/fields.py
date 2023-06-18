8dad43b6efdc6c34e74d0b3e14865462dd1314f1
Nina Menezes <nina.menezes@cabinetoffice.gov.uk>
2023-06-18 20:36:31 +0100

    tidy

--- etf/evaluation/fields.py
+++ etf/evaluation/fields.py
@@ -240,7 +240,8 @@ studied_population_help_text = defaultdict(
 title_help_text = defaultdict(
     str,
     {
-        "title": "A full/descriptive title for the evaluation, include a name or description of the interventions being evaluated. Spell out any abbreviations unless they are very familiar (eg NHS)."},
+        "title": "A full/descriptive title for the evaluation, include a name or description of the interventions being evaluated. Spell out any abbreviations unless they are very familiar (eg NHS)."
+    },
 )
 
 
